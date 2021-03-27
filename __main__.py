aee32eb4baf2f73a647068bab16ed9c30d27b2f3
ashwinvin <ashwinvinodsa@gmail.com>
2021-03-28 01:13:53 +0530

    yea

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import logging
 import discord
 import asyncpg
+from analyst.help import AnalystHelp
 import asyncio
 from analyst.bot import Analyst, loadall
 
@@ -10,7 +11,8 @@ config = BotConfig()
 db = asyncio.get_event_loop().run_until_complete(asyncpg.create_pool(dsn=config.PGDSN))
 logger = logging.getLogger(__name__)
 
-bot = Analyst(command_prefix="a!", intents=discord.Intents.all(), db=db)
+bot = Analyst(command_prefix="a!", intents=discord.Intents.all(), db=db, error_channel=config.CError)
+bot.help_command = AnalystHelp()
 
 loadall(bot)
 
