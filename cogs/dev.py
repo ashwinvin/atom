19c3252222cb00313f220153a99e948a3d192dca
ashwinvin <ashwinvinodsa@gmail.com>
2021-03-29 09:36:50 +0530

    update emoji again

--- cogs/dev.py
+++ cogs/dev.py
@@ -55,7 +55,7 @@ class DevTools(commands.Cog):
         description = ""
         await first.delete()
         for cog in temp:
-            description += f"\n Reloaded {cog} <:on1:825942418004246578>"
+            description += f"\n Reloaded {cog} <:on1:824388581433933896>"
         if description != "":
             await ctx.send(embed=self.bot.embed(description=description))
         else:
