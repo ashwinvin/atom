e9686a263cbd16ea8148cbe5397fb39f6d69a5ff
ashwinvin <ashwinvinodsa@gmail.com>
2021-03-29 16:46:04 +0530

    update sql

--- setup.sql
+++ setup.sql
@@ -5,3 +5,9 @@ CREATE TABLE errors (id SERIAL PRIMARY KEY,
                     url TEXT NOT NULL,
                     type TEXT NOT NULL,
                     fixed BOOL NOT NULL);
+
+CREATE TABLE guilds (id SERIAL PRIMARY KEY,
+                    gid INT NOT NULL,
+                    samp TEXT NOT NULL,
+                    sbchannel INT NOT NULL,
+                    sbmin INT NOT NULL);
