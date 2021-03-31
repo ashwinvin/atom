000f0741aadef12a21bbc16e5550925c2a442626
ashwinvin <ashwinvinodsa@gmail.com>
2021-03-31 23:34:26 +0530

    new commands!!

--- cogs/guilds.py
+++ cogs/guilds.py
@@ -1,6 +1,7 @@
 import discord
 import typing
 import asyncio
+from discord.enums import PremiumType
 from discord.ext import commands
 
 
@@ -13,21 +14,39 @@ class GuildManagement(commands.Cog):
     async def on_message(self, message: discord.Message):
         if message.author.id in self.afkers.keys():
             self.afkers.pop(message.author.id)
-            await message.channel.send(embed=self.bot.embed(description=f"Welcome back {message.author.mention}", colorful=False))
+            await message.channel.send(
+                embed=self.bot.embed(
+                    description=f"Welcome back {message.author.mention}", colorful=False
+                )
+            )
             try:
                 await message.author.edit(nick=message.author.display_name[5:])
             except discord.Forbidden:
-                await message.channel.send("Failed to change your nickname!! Permissions Denied!!", delete_after=10)
-        if n := discord.utils.find(lambda n: n.id in self.afkers.keys(), message.mentions):
-            await message.reply(embed=self.bot.embed(description=f"Sorry but `{n.display_name}` is afk \n Reason:```{self.afkers[n.id]}```"))
+                await message.channel.send(
+                    "Failed to change your nickname!! Permissions Denied!!",
+                    delete_after=10,
+                )
+        if n := discord.utils.find(
+            lambda n: n.id in self.afkers.keys(), message.mentions
+        ):
+            await message.reply(
+                embed=self.bot.embed(
+                    description=f"Sorry but `{n.display_name}` is afk \n Reason:```{self.afkers[n.id]}```"
+                )
+            )
 
     @commands.has_permissions(administrator=True)
     @commands.command()
-    async def poll(self, ctx, description: str, choice1: str, choice2: str ):
+    async def poll(self, ctx, description: str, choice1: str, choice2: str):
         """"""
-        description = f"{description} \n \n \U0001f170 {choice1} \n \U0001f171 {choice2}" 
+        description = (
+            f"{description} \n \n \U0001f170 {choice1} \n \U0001f171 {choice2}"
+        )
         poll_embed = self.bot.embed(
-            title="Poll", color=0x77fc03, description="".join(description), colorful=False
+            title="Poll",
+            color=0x77FC03,
+            description="".join(description),
+            colorful=False,
         )
         poll_embed.set_footer(text=f"Poll started by {ctx.author.name}")
         msg = await ctx.send(embed=poll_embed)
@@ -38,11 +57,18 @@ class GuildManagement(commands.Cog):
     async def afk(self, ctx, *reason: typing.Optional[str]):
         if not reason:
             reason = "Unknown"
-        await ctx.send(embed=self.bot.embed(description=f"I have set your afk status to `{''.join(reason)}`", colorful=False))
+        await ctx.send(
+            embed=self.bot.embed(
+                description=f"I have set your afk status to `{''.join(reason)}`",
+                colorful=False,
+            )
+        )
         try:
             await ctx.author.edit(nick=f"[AFK]{ctx.author.display_name}")
         except discord.Forbidden:
-            await ctx.send("Failed to change your nickname!! Permissions Denied!!", delete_after=10)
+            await ctx.send(
+                "Failed to change your nickname!! Permissions Denied!!", delete_after=10
+            )
         await asyncio.sleep(2)
         self.afkers[ctx.author.id] = "".join(reason)
 
@@ -51,42 +77,69 @@ class GuildManagement(commands.Cog):
         description = ""
         for member in ctx.guild.members:
             if member.bot:
-                description += f"{member.mention} joined at {str(member.joined_at.strftime('%Y:%m:%d'))} \n" 
+                description += f"{member.mention} joined at {str(member.joined_at.strftime('%Y:%m:%d'))} \n"
                 pass
-            
-        embed = self.bot.embed(title=f"Bots in {ctx.guild.name}", description=description)
+
+        embed = self.bot.embed(
+            title=f"Bots in {ctx.guild.name}", description=description
+        )
         await ctx.send(embed=embed)
-        
-    @commands.command()
-    async def info(
+
+    @commands.command(name="server-info")
+    async def server_info(self, ctx):
+        embed = self.bot.embed()
+        embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
+        embed.add_field(
+            name="Owner",
+            value=f"{ctx.guild.owner.name}#{ctx.guild.owner.discriminator}",
+        )
+        embed.add_field(name="Region", value=ctx.guild.region)
+        embed.add_field(name="Members", value=ctx.guild.member_count)
+        embed.add_field(
+            name="Channels",
+            value=f"Text Channels: {len(ctx.guild.text_channels)} \n Voice Channels{len(ctx.guild.voice_channels)}",
+        )
+        embed.add_field(name="Emojis", value=f"Total {len(ctx.guild.emojis)} emojis")
+        embed.add_field(
+            name="Boost Status",
+            value=f"This Guild is boosted with {ctx.guild.premium_subscription_count} and has achieved level {ctx.guild.premium_tier} boost status"
+            if ctx.guild.premium_subscribers
+            else "This server is not boosted",
+        )
+        embed.add_field(name="Roles", value=len(ctx.guild.roles))
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="server-info")
+    async def user_info(
         self,
         ctx: commands.Context,
-        inquiry: typing.Optional[typing.Union[discord.Member, discord.Role]],
+        user: typing.Optional[discord.Member],
     ):
-        if not inquiry:
-            embed = self.bot.embed()
-            embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
-            embed.add_field(
-                name="Owner",
-                value=f"{ctx.guild.owner.name}#{ctx.guild.owner.discriminator}",
-            )
-            embed.add_field(name="Region", value=ctx.guild.region)
-            embed.add_field(name="Members", value=ctx.guild.member_count)
-            embed.add_field(
-                name="Channels",
-                value=f"Text Channels: {len(ctx.guild.text_channels)} \n Voice Channels{len(ctx.guild.voice_channels)}",
-            )
-            embed.add_field(
-                name="Emojis", value=f"Total {len(ctx.guild.emojis)} emojis"
-            )
-            embed.add_field(name="Roles", value=len(ctx.guild.roles))
-            embed.set_thumbnail(url=ctx.guild.icon_url)
-            await ctx.send(embed=embed)
-        if type(inquiry) == discord.Member:
-            embed = self.bot.embed()
-            embed.set_author(name=inquiry.display_name, icon_url=inquiry.avatar_url)
-            
-            pass
+        user = ctx.author if not user else user
+        embed = self.bot.embed()
+        embed.set_author(name=user.display_name, icon_url=user.avatar_url)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name="Roles", value=f"{len(user.roles)} roles")
+        embed.add_field(
+            name="Joined Server At",
+            value=f"{user.joined_at.strftime('%A %b %Y %H:%M')}",
+        )
+        embed.add_field(
+            name="Joined Discord At",
+            value=f"{user.created_at.strftime('%A %b %Y %H:%M')}",
+        )
+
+        embed.add_field(
+            name="Permissions",
+            value="".join(
+                filter(
+                    lambda a: True if not "__" in a else False,
+                    dir(user.guild_permissions),
+                )
+            ),
+        )
+        await ctx.reply(embed=embed)
 
 
 def setup(bot):
