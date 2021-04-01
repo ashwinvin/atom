90707c382ae940d4ffc9f0ea7ae3d76d555cf04f
ashwinvin <ashwinvinodsa@gmail.com>
2021-04-02 02:19:51 +0530

    debug

--- cogs/dev.py
+++ cogs/dev.py
@@ -83,18 +83,19 @@ class DevTools(commands.Cog):
     @commands.is_owner()
     @commands.command()
     async def sql(self, ctx, *query: str):
-        try:
+        # try:
+            
             async with self.bot.db.acquire() as conn:
                 async with conn.transaction():
                     results = await conn.fetch(" ".join(query))
                     table = beautifultable.BeautifulTable()
-                    table.columns.header = list(results[0].keys())
+                    table.columns.header = list(results[0].keys()) 
                     for result in results:
                         for key, value in result.items():    
                             table.rows.append(value, header=key)
                     await ctx.send(table)
-        except Exception as e:
-            await ctx.send(f"Sql Statement failed due to \n {e}")
+        # except Exception as e:
+            # await ctx.send(f"Sql Statement failed due to \n {e}")
 
 def setup(bot):
     bot.add_cog(DevTools(bot))
