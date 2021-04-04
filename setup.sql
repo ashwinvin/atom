5012749e75eeb3208ac4a1712291f1265fdd14aa
ashwinvin <ashwinvinodsa@gmail.com>
2021-04-04 23:25:06 +0530

    better database

--- setup.sql
+++ setup.sql
@@ -1,16 +1,18 @@
-CREATE DATABASE analyst;
-\c analyst ;
+CREATE TABLE IF NOT EXISTS errors (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    type TEXT NOT NULL,
+    fixed BOOL NOT NULL
+);
 
-CREATE TABLE errors (id SERIAL PRIMARY KEY, 
-                    url TEXT NOT NULL,
-                    type TEXT NOT NULL,
-                    fixed BOOL NOT NULL);
+CREATE TABLE IF NOT EXISTS guilds (
+    id SERIAL PRIMARY KEY,
+    gid BIGINT NOT NULL UNIQUE,
+    prefix TEXT DEFAULT 'sh!' NOT NULL
+);
 
-CREATE TABLE guilds (id SERIAL PRIMARY KEY,
-                    gid BIGINT NOT NULL UNIQUE,
-                    samp_ip TEXT,
-                    samp_port INT,
-                    sbchannel BIGINT ,
-                    sbmin INT);
-
- 
+CREATE TABLE IF NOT EXISTS samp (
+    id INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
+    samp_ip TEXT,
+    samp_port INT
+);
