1c068ac3eeaba906e341cb1a031a2ffc266f4762
ashwinvin <ashwinvinodsa@gmail.com>
2021-04-24 23:42:00 +0530

    format and more fields to errors table

--- setup.sql
+++ setup.sql
@@ -1,41 +1,37 @@
 CREATE TABLE IF NOT EXISTS errors (
-    id SERIAL PRIMARY KEY,
-    url TEXT NOT NULL,
-    type TEXT NOT NULL,
-    fixed BOOL NOT NULL
+    id              SERIAL PRIMARY KEY,
+    url             TEXT NOT NULL,
+    type            TEXT NOT NULL,
+    msg             TEXT NOT NULL,
+    user            BIGINT NOT NULL,
+    fixed           BOOL NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS guilds (
-    id SERIAL PRIMARY KEY,
-    gid BIGINT NOT NULL UNIQUE,
-    prefix TEXT DEFAULT 'sh!' NOT NULL
+    id              SERIAL PRIMARY KEY,
+    gid             BIGINT NOT NULL UNIQUE,
+    prefix          TEXT DEFAULT '#' NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS samp (
-    id INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
-    samp_ip TEXT,
-    samp_port INT
+    id             INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
+    samp_ip        TEXT,
+    samp_port      INT
 );
 
 
 CREATE TABLE IF NOT EXISTS minecraft (
-    id INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
-    mc_ip TEXT,
-    mc_port INT
+    id            INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
+    mc_ip         TEXT,
+    mc_port       INT
 );
 
 CREATE TABLE IF NOT EXISTS tags (
-    id SERIAL PRIMARY KEY,
-    gid BIGINT NOT NULL REFERENCES guilds(gid) ON DELETE CASCADE,
-    author BIGINT NOT NULL,
-    name TEXT NOT NULL,
-    content TEXT NOT NULL,
-    public BOOLEAN DEFAULT false,
-    allowed BIGINT []
+    id            SERIAL PRIMARY KEY,
+    gid           BIGINT NOT NULL REFERENCES guilds(gid) ON DELETE CASCADE,
+    author        BIGINT NOT NULL,
+    name          TEXT NOT NULL,
+    content       TEXT NOT NULL,
+    public        BOOLEAN DEFAULT false,
+    allowed       BIGINT []
 );
-
-CREATE TABLE IF NOT EXISTS whitelist(
-    id INT PRIMARY KEY REFERENCES guilds(id) ON DELETE CASCADE,
-    channel BIGINT NOT NULL,
-    roles BIGINT [] NOT NULL
-)
