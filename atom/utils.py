d5cf6823674a3c2922ca05fe0553ba3ace95f505
ashwinvin <ashwinvinodsa@gmail.com>
2021-04-29 14:12:09 +0530

    utility functions

---
+++ atom/utils.py
@@ -0,0 +1,13 @@
+from functools import partial, wraps
+import asyncio
+import functools
+
+def executor(loop=None):
+    loop = loop or asyncio.get_event_loop()
+    def inner(func):    
+        @wraps(func)
+        def function(*args, **kwargs):
+            fpartial = partial(func, *args, **kwargs)
+            return loop.run_in_executor(None, fpartial)
+        return function
+    return inner
